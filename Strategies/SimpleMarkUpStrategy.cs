a330defb3f914d978215b53f46744a27a6fdedf7
Ulises Cadena <ucadena07@gmail.com>
2020-05-27 10:31:49 -0500

    strategy

---
+++ Strategies/SimpleMarkUpStrategy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using SudokuSolver.workers;
+using System.Linq;
+
+namespace SudokuSolver.Strategies
+{
+    public class SimpleMarkUpStrategy : ISudokuStragedy
+    {
+        private readonly SudokuMapper _sudokuMapper;
+
+        public SimpleMarkUpStrategy(SudokuMapper sudokuMapper)
+        {
+            _sudokuMapper = sudokuMapper;
+        }
+
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    if (sudokuBoard[row, col] == 0 || sudokuBoard[row, col].ToString().Length > 1)
+                    {
+                        var possibilitiesInRowAndCol = GetPossiblilitiesInRowAndCol(sudokuBoard, row, col);
+                        var possibilitiesInBlock = GetPossibilitiesInBlock(sudokuBoard, row, col);
+                        sudokuBoard[row, col] = GetPossibilityIntersection(possibilitiesInRowAndCol, possibilitiesInBlock);
+                    }
+                }
+                
+            }
+
+
+
+            return sudokuBoard;
+        }
+
+        private object GetPossiblilitiesInRowAndCol(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            int[] possibilities = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int col = 0; col < 9; col++)
+            {
+                if(IsValidSingle(sudokuBoard[givenRow,col]))
+                {
+                    possibilities[sudokuBoard[givenRow, col] - 1] = 0;
+                }
+            }
+
+            for (int row = 0; row < 9; row++)
+            {
+                if (IsValidSingle(sudokuBoard[row, givenCol]))
+                {
+                    possibilities[sudokuBoard[row, givenCol] - 1] = 0;
+                }
+            }
+            return Convert.ToInt32(String.Join(string.Empty, possibilities.Select(p => p).Where(p => p != 0)));
+        }
+
+        private bool IsValidSingle(int cellDigit)
+        {
+            return cellDigit != 0 && cellDigit.ToString().Length == 1;
+        }
+
+        private object GetPossibilitiesInBlock(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetPossibilityIntersection(object possibilitiesInRowAndCol, object possibilitiesInBlock)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
--- Strategies/SudokuSolverEngine.cs
+++ Strategies/SudokuSolverEngine.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SudokuSolver.workers;
+
 namespace SudokuSolver.Strategies
 {
     public class SudokuSolverEngine
     {
-        public SudokuSolverEngine()
+        private readonly SudokuBoardStateManager _sudokuBoardStateManager;
+        private readonly SudokuMapper _sudokuMapper;
+
+        public SudokuSolverEngine(SudokuBoardStateManager sudokuBoardStateManager, SudokuMapper sudokuMapper)
         {
+            _sudokuBoardStateManager = sudokuBoardStateManager;
+            _sudokuMapper = sudokuMapper;
+
+        }
+
+        public bool Solve(int[,] sudokuBoard) 
+        {
+            List<ISudokuStragedy> stragedies = new List<ISudokuStragedy>()
+            {
+
+            };
+
+            var currentState = _sudokuBoardStateManager.GenerateState(sudokuBoard);
+            var nextState = _sudokuBoardStateManager.GenerateState(stragedies.First().Solve(sudokuBoard));
+
+            while (!_sudokuBoardStateManager.IsSolved(sudokuBoard) && currentState != nextState)
+            {
+                currentState = nextState;
+                foreach (var strategy in stragedies)
+                {
+                    nextState = _sudokuBoardStateManager.GenerateState(strategy.Solve(sudokuBoard));
+                }
+            }
+            return _sudokuBoardStateManager.IsSolved(sudokuBoard);
         }
     }
 }
