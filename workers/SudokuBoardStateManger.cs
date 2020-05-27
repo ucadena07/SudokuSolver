c0675f25cac5c044884837485f593d6ebfafdea3
Ulises Cadena <ucadena07@gmail.com>
2020-05-27 09:04:10 -0500

    sudoku state manger

---
+++ workers/SudokuBoardStateManger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace SudokuSolver.workers
+{
+    public class SudokuBoardStateManger
+    {
+        public string GenerateState(int[,] sudokuBoard)
+        {
+            StringBuilder key = new StringBuilder();
+
+
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    key.Append(sudokuBoard[row, col]);
+                }
+            }
+            return key.ToString();
+        }
+
+        public bool IsSolved(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    if (sudokuBoard[row, col] == 0 || sudokuBoard[row, col].ToString().Length > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
