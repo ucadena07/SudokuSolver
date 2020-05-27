2d4275bf8464c01ff9916236c5ea9a71a7194bfb
Ulises Cadena <ucadena07@gmail.com>
2020-05-27 08:49:00 -0500

    Display Method

---
+++ workers/SudokuBoardDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SudokuSolver.workers
+{
+    public class SudokuBoardDisplayer
+    {
+        public void Display(string title, int[,] sudokuBoard)
+        {
+            if (!title.Equals(string.Empty)) Console.WriteLine("{0} {1}", title, Environment.NewLine);
+
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                Console.Write("|");
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    Console.WriteLine("{0} {1}", sudokuBoard[row, col], "|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
